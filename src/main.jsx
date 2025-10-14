f3358526cc6e2e9cda6cbca46305947bd1e25e49
Yorran Godoy <yorran.godoy@gmail.com>
2025-10-14 17:47:51 -0300

    chore: stop tracking artifacts (post-merge)

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App.jsx';
+import App from './app.jsx';
 import './styles/global.css';
 
 createRoot(document.getElementById('root')).render(
